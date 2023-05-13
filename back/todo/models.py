67210dd59564bffdd72947d19378d14afa687dd5
石田樹 <ishidaitsuki@ssltest.ochin.org>
2023-05-13 13:51:59 +0900

    models.pyを編集しました

--- back/todo/models.py
+++ back/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
+class Task(models.Model): #idとcreated_at、updated_atは必ず入れる
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) #これがprimarykeyですよ/なにも設定しなくても勝手に設定/変更できない
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=500) #contentsって言われると配列をイメージする、複数形は配列のイメージ
+    created_at = models.DateTimeField(auto_now_add=True) #この名前はテンプレ #新規作成した時に勝手に設定/
+    updated_at = models.DateTimeField(auto_now=True)
 # Create your models here.
