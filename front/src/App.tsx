dc47330b03b1552a8fd1beb1602a0ac9b156f0fb
sotamizukami <132802172+sotamizukami@users.noreply.github.com>
2023-05-17 22:30:21 +0900

    ボタンの作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { borderRadius } from '@mui/system';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Stack
+        direction = 'row'
+        spacing ={3}
+        p = {2}
+        sx={{
+          width: 300,
+          height: 50,
+          outlineColor: 'black',
+          border: '2px solid #000000',
+          borderRadius: '10px'
+        }}>
+        <Box
+          sx={{
+              justifyContent: 'center'
+            }}>
+          <Typography>
+          Reactの勉強をする
+          </Typography>
+        </Box>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
     </div>
-  );
+  ) 
 }
 
 export default App;
