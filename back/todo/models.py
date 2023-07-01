ec4ca1e06121f4dd61419451b4be29affa84f982
gunmaken <asada0127@icloud.com>
2023-07-01 19:07:30 +0900

    edited models, serializer, urls, views

--- back/todo/models.py
+++ back/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.title
--- back/todo/views.py
+++ back/todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+
+    queryset = Todo.objects.all()
+    serializer = TodoSerializer
+    permission_classes = [AllowAny]
